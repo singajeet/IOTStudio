107bb304d35cc74f250e840fb8926cd5d885cf24
singajeet <singajeet@gmail.com>
2018-01-16 15:37:34 +0530

    changes in admin models

--- gui/gui_models/tags.py
+++ gui/gui_models/tags.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
+import os
+from IOTStudio.settings import BASE_DIR
 from . import iot
 
 
@@ -16,18 +18,32 @@ class HtmlTag(models.Model):
     security_id = models.ForeignKey(iot.SecurityIdModel, models.SET_NULL, blank = True, null = True)
 
 class StyleTag(models.Model):
+    STYLE_TYPES=(
+            ('-1', 'None'),
+            ('00', 'File'),
+            ('01', 'Inline'),
+            )
     id = models.AutoField(primary_key=True)
     tag_name = models.CharField(max_length=255, unique = True)
-    style_text = models.TextField()
+    style_type = models.CharField(max_length=2, choices=STYLE_TYPES, default='00')
+    style_text = models.TextField(blank=True, null=True)
+    style_file = models.FilePathField(path=os.path.join(BASE_DIR, 'staticfiles/css'), recursive = True, allow_folders = False, blank = True, null = True)
     created_on_date = models.DateTimeField(auto_now_add = True, editable = False)
     modified_on_date = models.DateTimeField(auto_now = True, editable = False)
     author = models.ForeignKey(User, blank=True, null=True)
     security_id = models.ForeignKey(iot.SecurityIdModel, models.SET_NULL, blank = True, null = True)
 
 class ScriptTag(models.Model):
+    SCRIPT_TYPES=(
+            ('-1', 'None'),
+            ('00', 'File'),
+            ('01', 'Inline'),
+            )
     id = models.AutoField(primary_key=True)
     tag_name = models.CharField(max_length=255, unique = True)
-    script_text = models.TextField()
+    script_type = models.CharField(max_length = 2, choices=SCRIPT_TYPES, default='00')
+    script_file = models.FilePathField(path=os.path.join(BASE_DIR, 'staticfiles/js'), recursive=True, allow_folders=False, blank=True, null=True)
+    script_text = models.TextField(blank=True, null=True)
     created_on_date = models.DateTimeField(auto_now_add = True, editable = False)
     modified_on_date = models.DateTimeField(auto_now = True, editable = False)
     author = models.ForeignKey(User, blank=True, null=True)
