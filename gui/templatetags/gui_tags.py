89aa34a9bf0cac6e88d80274fad307441e71020c
singajeet <singajeet@gmail.com>
2018-01-17 18:26:15 +0530

    In memory HTML template implemented

--- gui/templatetags/gui_tags.py
+++ gui/templatetags/gui_tags.py
@@ -2,70 +2,37 @@
 from __future__ import unicode_literals
 from ..gui_models import tags
 from django import template
-from django.utils.html import format_html
 from django.shortcuts import get_object_or_404
-from django.utils.safestring import mark_safe
-from django.templatetags.static import static
-from IOTStudio.settings import STATIC_ROOT
+from .. import utilities
 
 register = template.Library()
 
 @register.simple_tag
-def title(title_text):
-    if title_text is None:
-        return '-EmptyTitle-'
-        
-    html_tag = get_object_or_404(tags.HtmlTag, tag_name='Title')
-    if html_tag is None:
-        return '-EmptyTitle-'
-        
-    return format_html(('<!-- TitleTag -->\n\t<title>{0}</title>'.format(html_tag.tag_text)), title_text)
+def title(title_name):
+    title_tag = get_object_or_404(tags.HtmlTag, tag_name=title_name)
+    
+    return utilities.get_title_tag_html(title_tag)
 
 @register.simple_tag
 def style(style_name):
-    if style_name is not None:
-        style_tag = get_object_or_404(tags.StyleTag, tag_name=style_name)
-        if style_tag is not None and style_tag.style_type == '00':
-            file_name = style_tag.style_file.replace(STATIC_ROOT, '')            
-            return format_html('<!-- StyleTag -->\n\t<link href="{0}" rel="stylesheet" />'.format(static(file_name)))
-        
-        if style_tag is not None and style_tag.style_type == '01':
-            return format_html('<!-- StyleTag -->\n\t<style>\n\t{0}\n\t</style>'.format(style_tag.style_text))
-            
-    return None
+    style_tag = get_object_or_404(tags.StyleTag, tag_name=style_name)
+    
+    return utilities.get_style_tag_html(style_tag)
     
 @register.simple_tag
 def script(script_name):
-    if script_name is not None:
-        script_tag = get_object_or_404(tags.ScriptTag, tag_name=script_name)
+    script_tag = get_object_or_404(tags.ScriptTag, tag_name=script_name)
         
-        if script_tag is not None and script_tag.script_type == '00':
-            file_name = script_tag.script_file.replace(STATIC_ROOT, '')
-            return format_html('<!-- ScriptTag -->\n\t<script src="{0}" type="application/javascript"></script>' . format(static(file_name)))
-
-        if script_tag is not None and script_tag.script_type == '01':
-            return format_html('<!-- ScriptTag -->\n\t<script>\n\t{0}\n\t</script>'.format(script_tag.script_text))
-            
-        if script_tag is not None and script_tag.script_type == '02':
-            return format_html('<!-- ScriptTag -->\n\t<script src="{0}" type="application/javascript"></script>' . format(script_tag.script_url))
-
-    return None
+    return utilities.get_script_tag_html(script_tag)
 
 @register.simple_tag
-def meta():
-    meta_tag = get_object_or_404(tags.HtmlTag, tag_name='Meta')
-    if meta_tag is not None:
-        return format_html('<!-- MetaTag -->\n\t{0}'.format(meta_tag.tag_text))
+def meta(meta_name):
+    meta_tag = get_object_or_404(tags.HtmlTag, tag_name=meta_name)
+    
+    return utilities.get_meta_tag_html(meta_tag)
 
 @register.simple_tag
 def page_icon(icon_name):
     icon_tag = get_object_or_404(tags.IconTag, tag_name=icon_name)
     
-    if icon_tag is not None and icon_tag.icon_type == '00':
-        file_name = icon_tag.icon_file.replace(STATIC_ROOT, '')
-        return format_html('<!-- PageIconTag -->\n\t<link rel="shortcut icon" href="{0}" />'.format(static(file_name)))
-    
-    if icon_tag is not None and icon_tag.icon_type == '01':
-        return format_html('<!-- PageIconTag -->\n\t{0}'.format(icon_tag.icon_text))
-        
-    return None
+    return utilities.get_page_icon_html(icon_tag)
--- gui/views.py
+++ gui/views.py
@@ -1,13 +1,60 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django import shortcuts
 from django.http import HttpResponse
+from django.template import Template, Context
 from . import models
+from . import utilities
 # Create your views here.
+
+TEMPLATE_NAME = '<!-- Template Name: {0} -->'
+NEW_LINE = '\n'
+HTML_START = '<!DOCTYPE html>' + NEW_LINE + '<html lang="en">' + NEW_LINE
+HTML_END = '</html>' + NEW_LINE
+HEAD_START = '<head>' + NEW_LINE
+HEAD_END = '</head>' + NEW_LINE
+BODY_START = '<body>' + NEW_LINE
+BODY_END = '</body>' + NEW_LINE
 
 def index(request):
     app_details = models.ApplicationModel.objects.get(id=2)
     template = models.TemplateModel.objects.get(id=app_details.selected_template.id)
     context = { 'app_details' : app_details, }
-    return render(request, template.template_path.lstrip('\\'), context)
+    
+    if template is not None and template.template_type == '00':
+        return shortcuts.render(request, template.template_path.lstrip('\\'), context)
+        
+    if template is not None and template.template_type == '01':
+        #start HTML tag
+        html = TEMPLATE_NAME.format(template.name)
+        html += HTML_START
+        
+        #start Head tag
+        html += HEAD_START
+        
+        #Render all meta tags if available
+        html += utilities.get_all_meta_tags_html(template.meta_tags) + NEW_LINE
+        
+        #Render page-icon-tag
+        html += utilities.get_page_icon_html(template.page_icon_tag) + NEW_LINE
+        
+        #Render title-tag 
+        html += utilities.get_title_tag_html(template.title_tag) + NEW_LINE
+        
+        #Render header-script-tags
+        html += utilities.get_all_script_tags_html(template.header_script_tags) + NEW_LINE
+        
+        #Render header-style-tags
+        html += utilities.get_all_style_tags_html(template.header_style_tags)
+        
+        
+        html += HEAD_END
+        html += BODY_START
+        
+        html += utilities.get_all_script_tags_html(template.header_script_tags) + NEW_LINE
+        html += BODY_END
+        html += HTML_END
+        html_template = Template(html)
+        html_context = Context(context)
+        return HttpResponse(html_template.render(html_context))
