48785a9d97506754e21aab79180a04d3b633ee6f
singajeet <singajeet@gmail.com>
2018-01-21 12:15:12 +0530

    Implemented sorting

--- gui/models.py
+++ gui/models.py
@@ -9,9 +9,44 @@ import os
 from polymorphic.models import PolymorphicModel
 from django.db.models.query_utils import Q
 
+NONE = 'None'
+DIV = 'Div'
+SPAN = 'Span'
+NAV = 'nav'
+HEADER = 'header'
+MAIN = 'main'
+HTML = 'Html'
+PANEL = 'Panel'
+STACK_PANEL = 'StackPanel'
+FLOW_LAYOUT_PANEL = 'FlowLayoutPanel'
+WRAP_PANEL = 'WrapPanel'
+DOCK_PANEL = 'DockPanel'
+GRID_PANEL = 'GridPanel'
+ABSOLUTE_PANEL = 'AbsolutePanel'
+FLYOUT_PANEL = 'FlyoutPanel'
+
+SORT_LEVEL = 'sort_level'
+
+FILE = 'File'
+RUNTIME = 'Runtime'
+PAGE_ICON = 'page_icon'
+INLINE = 'Inline'
+
+_MINUS_1 = '-1'
+_00 = '00'
+_01 = '01'
+_02 = '02'
+_03 = '03'
+_04 = '04'
+_05 = '05'
+_06 = '06'
+
+START_TAG_TEMPLATE = '<{0} id="{1}" class="{2}" >'
+END_TAG_TEMPLATE = '</{0}>'
+
 TEMPLATE_NAME = '<!-- Template Name: {0} -->'
 NEW_LINE = '\n'
-HTML_START = '<!DOCTYPE html>' + NEW_LINE + '<html lang="en">' + NEW_LINE
+HTML_START = NEW_LINE + '<!DOCTYPE html>' + NEW_LINE + '<html lang="en">' + NEW_LINE
 HTML_END = '</html>' + NEW_LINE
 HEAD_START = '<head>' + NEW_LINE
 HEAD_END = '</head>' + NEW_LINE
@@ -42,16 +77,16 @@ class UIElementModel(iot.BaseModel):
 
 class PanelModel(UIElementModel):
     PANEL_TYPES=(
-            ('-1', 'Panel'),
-            ('00', 'StackPanel'),
-            ('01', 'FlowLayoutPanel'),
-            ('02', 'WrapPanel'),
-            ('03', 'DockPanel'),
-            ('04', 'GridPanel'),
-            ('05', 'AbsolutePanel'),
-            ('06', 'FlyoutPanel'),
+            (_MINUS_1, PANEL),
+            (_00, STACK_PANEL),
+            (_01, FLOW_LAYOUT_PANEL),
+            (_02, WRAP_PANEL),
+            (_03, DOCK_PANEL),
+            (_04, GRID_PANEL),
+            (_05, ABSOLUTE_PANEL),
+            (_06, FLYOUT_PANEL),
             )
-    panel_type = models.CharField(max_length=2, blank = True, choices = PANEL_TYPES, default = '-1')
+    panel_type = models.CharField(max_length=2, blank = True, choices = PANEL_TYPES, default = _MINUS_1)
     parent_id = models.ForeignKey('self', models.SET_NULL, blank = True, null = True)
     children = models.ManyToManyField(UIElementModel, blank = True, related_name = '+', default = None) 
     author = models.ForeignKey(User, models.SET_NULL, blank = True, null = True)
@@ -68,17 +103,17 @@ class PanelModel(UIElementModel):
 
 class PlaceHolderModel(iot.BaseModel):
     PLACE_HOLDER_TYPES=(
-            ('-1', 'None'),
-            ('00', 'HTML'),
-            ('01', 'Panel'),
+            (_MINUS_1, NONE),
+            (_00, HTML),
+            (_01, PANEL),
             )
     TAG_TYPES=(
-            ('-1', 'None'),
-            ('00', 'Div'),
-            ('01', 'Span'),
+            (_MINUS_1, NONE),
+            (_00, DIV),
+            (_01, SPAN),
             )
-    place_holder_type = models.CharField(max_length=2, choices = PLACE_HOLDER_TYPES, default='00')
-    tag_type = models.CharField(max_length=2, choices=TAG_TYPES, default='00')
+    place_holder_type = models.CharField(max_length=2, choices = PLACE_HOLDER_TYPES, default=_00)
+    tag_type = models.CharField(max_length=2, choices=TAG_TYPES, default=_00)
     css_classes = models.CharField(max_length=100, blank=True, null=True)
     author = models.ForeignKey(User, models.SET_NULL, blank = True, null = True)
 
@@ -103,20 +138,20 @@ class HtmlPlaceHolderModel(PlaceHolderModel):
         
     def build_html(self):
         html = ''
-        pre_tag = '<{0} id="{1}" class="{2}" >'
-        post_tag = '</{0}>'
-        if self.tag_type == '00':
-            pre_tag = pre_tag.format('div', self.slug, self.css_classes)
-            post_tag = post_tag.format('div')
-        if self.tag_type == '01':
-            pre_tag = pre_tag.format('span', self.slug, self.css_classes)
-            post_tag = post_tag.format('span')
-        if self.tag_type != '-1':
+        pre_tag = START_TAG_TEMPLATE
+        post_tag = END_TAG_TEMPLATE
+        if self.tag_type == _00:
+            pre_tag = pre_tag.format(DIV, self.slug, self.css_classes)
+            post_tag = post_tag.format(DIV)
+        if self.tag_type == _01:
+            pre_tag = pre_tag.format(SPAN, self.slug, self.css_classes)
+            post_tag = post_tag.format(SPAN)
+        if self.tag_type != _MINUS_1:
             html += pre_tag
-        for html_tag in self.html_tags.all().order_by('sort_level'):
+        for html_tag in self.html_tags.all().order_by(SORT_LEVEL):
             html += html_tag.get_html()
             
-        if self.tag_type != '-1':
+        if self.tag_type != _MINUS_1:
             html += post_tag
 
         return html
@@ -133,7 +168,7 @@ class PanelPlaceHolderModel(PlaceHolderModel):
 
     def build_html(self):
         html = ''
-        for panel in self.panels.all().order_by('sort_level'):
+        for panel in self.panels.all().order_by(SORT_LEVEL):
             html += panel.get_html()
             
         return html
@@ -144,11 +179,14 @@ class PanelPlaceHolderModel(PlaceHolderModel):
 
 class SectionModel(iot.BaseModel):
     TAG_TYPES=(
-            ('-1', 'None'),
-            ('00', 'Div'),
-            ('01', 'Span'),
+            (_MINUS_1, NONE),
+            (_00, DIV),
+            (_01, SPAN),
+            (_02, HEADER),
+            (_03, MAIN),
+            (_04, NAV),
             )
-    tag_type = models.CharField(max_length=2, choices=TAG_TYPES, default='00')
+    tag_type = models.CharField(max_length=2, choices=TAG_TYPES, default=_00)
     css_classes = models.CharField(max_length=100, blank=True, null=True)
     header_html = models.TextField(null = True, blank = True)
     place_holders = models.ManyToManyField(PlaceHolderModel, blank = True, related_name = '+', default = None, help_text = 'Each section consist of 0 or more place holders of type HTML or panel')
@@ -160,23 +198,40 @@ class SectionModel(iot.BaseModel):
         
     def get_html(self):
         html = ''
-        pre_tag = '<{0} id="{1}" class="{2}" >'
-        post_tag = '</{0}>'
-        if self.tag_type == '00':
-            pre_tag = pre_tag.format('div', self.slug, self.css_classes)
-            post_tag = post_tag.format('div')
-        if self.tag_type == '01':
-            pre_tag = pre_tag.format('span', self.slug, self.css_classes)
-            post_tag = post_tag.format('span')
-        if self.tag_type != '-1':
+        html += '<!-- Start [{0}] Section -->'.format(self.name) + NEW_LINE
+        pre_tag = START_TAG_TEMPLATE + NEW_LINE
+        post_tag = END_TAG_TEMPLATE
+        if self.tag_type == _00:
+            pre_tag = pre_tag.format(DIV, self.slug, self.css_classes)
+            post_tag = post_tag.format(DIV)
+        if self.tag_type == _01:
+            pre_tag = pre_tag.format(SPAN, self.slug, self.css_classes)
+            post_tag = post_tag.format(SPAN)
+        if self.tag_type == _02:
+            pre_tag = pre_tag.format(HEADER, self.slug, self.css_classes)
+            post_tag = post_tag.format(HEADER)
+        if self.tag_type == _03:
+            pre_tag = pre_tag.format(MAIN, self.slug, self.css_classes)
+            post_tag = post_tag.format(MAIN)
+        if self.tag_type == _04:
+            pre_tag = pre_tag.format(NAV, self.slug, self.css_classes)
+            post_tag = post_tag.format(NAV)
+        if self.tag_type != _MINUS_1:
             html += pre_tag
-        html += ('' if self.header_html is None else self.header_html) + NEW_LINE
-        for place_holder in self.place_holders.all().order_by('sort_level'):
+        #render header html if provided
+        html += '' if self.header_html is None else (self.header_html + NEW_LINE)
+        #render all place holders
+        for place_holder in self.place_holders.all().order_by(SORT_LEVEL):
             html += place_holder.get_html()
+        #Render new line if place holders were rendered
+        if self.place_holders.count() > 0:
+            html += NEW_LINE
+        #render footer html if provided
+        html += '' if self.footer_html is None else (self.footer_html + NEW_LINE)
+        if self.tag_type != _MINUS_1:
+            html += post_tag + NEW_LINE
             
-        html += ('' if self.footer_html is None else self.footer_html) + NEW_LINE
-        if self.tag_type != '-1':
-            html += post_tag
+        html += '<!-- End [{0}] Section -->'.format(self.name)
         return html
 
     class Meta:
@@ -185,18 +240,19 @@ class SectionModel(iot.BaseModel):
 
 class TemplateBlockModel(iot.BaseModel):
     TAG_TYPES=(
-            ('-1', 'None'),
-            ('00', 'Div'),
-            ('01', 'Span'),
-            ('02', 'Header'),
-            ('03', 'Main'),
+            (_MINUS_1, NONE),
+            (_00, DIV),
+            (_01, SPAN),
+            (_02, HEADER),
+            (_03, MAIN),
+            (_04, NAV),
             )
-    tag_type = models.CharField(max_length=2, choices=TAG_TYPES, default='00')
+    tag_type = models.CharField(max_length=2, choices=TAG_TYPES, default=_00)
     css_classes = models.CharField(max_length=100, blank=True, null=True)
     allow_change_in_child = models.BooleanField(default=False, help_text='If checked, block can be overwritten by child templates')
     header_html = models.TextField(null = True, blank = True)
     sections = models.ManyToManyField(SectionModel, blank = True, related_name='+', default=None, help_text='Each block consist of 0 or more sections')
-    child_blocks = models.ManyToManyField('self', blank=True, related_name='+', default=None)
+    child_blocks = models.ManyToManyField('self', blank=True, related_name='+', symmetrical = False, default=None)
     footer_html = models.TextField(null = True, blank = True)
     author = models.ForeignKey(User, models.SET_NULL, blank = True, null = True)
 
@@ -205,31 +261,49 @@ class TemplateBlockModel(iot.BaseModel):
 
     def get_html(self):
         html = ''
-        pre_tag = '<{0} id="{1}" class="{2}" >' + NEW_LINE          
-        post_tag = '</{0}>'
-        if self.tag_type == '00':
-            pre_tag = pre_tag.format('div', self.slug, self.css_classes)
-            post_tag = post_tag.format('div')
-        if self.tag_type == '01':
-            pre_tag = pre_tag.format('span', self.slug, self.css_classes)
-            post_tag = post_tag.format('span')
-        if self.tag_type == '02':
-            pre_tag = pre_tag.format('header', self.slug, self.css_classes)
-            post_tag = post_tag.format('header')
-        if self.tag_type == '03':
-            pre_tag = pre_tag.format('main', self.slug, self.css_classes)
-            post_tag = post_tag.format('main')
-        if self.tag_type != '-1':
+        #Render comment with block name
+        html += '<!-- Start [{0}] Block -->'.format(self.name) + NEW_LINE
+        #Render Pre/Post tags as containers for this block 
+        pre_tag = START_TAG_TEMPLATE + NEW_LINE          
+        post_tag = END_TAG_TEMPLATE
+        #Based on type of HTML tag selected, the pre part of block will be prepared to render
+        if self.tag_type == _00:
+            pre_tag = pre_tag.format(DIV, self.slug, self.css_classes)
+            post_tag = post_tag.format(DIV)
+        if self.tag_type == _01:
+            pre_tag = pre_tag.format(SPAN, self.slug, self.css_classes)
+            post_tag = post_tag.format(SPAN)
+        if self.tag_type == _02:
+            pre_tag = pre_tag.format(HEADER, self.slug, self.css_classes)
+            post_tag = post_tag.format(HEADER)
+        if self.tag_type == _03:
+            pre_tag = pre_tag.format(MAIN, self.slug, self.css_classes)
+            post_tag = post_tag.format(MAIN)
+        if self.tag_type == _04:
+            pre_tag = pre_tag.format(NAV, self.slug, self.css_classes)
+            post_tag = post_tag.format(NAV)
+        #if a valid html tag is selected, render it else nothing is rendered
+        if self.tag_type != _MINUS_1:
             html += pre_tag
-        html += ('' if self.header_html is None else self.header_html) + NEW_LINE
-        for section in self.sections.all().order_by('sort_level'):
+        #render the header  part of block if any html is provided in admin
+        html += '' if (self.header_html is None or self.header_html == '') else (self.header_html + NEW_LINE)
+        #render all the sections under this block if available
+        for section in self.sections.all().order_by(SORT_LEVEL):
             html += section.get_html()
-        for child_block in self.child_blocks.all().order_by('sort_level'):
+        #if sections were rendered, print a NewLine to have child blocks rendered in next line
+        if self.sections.count() > 0:
+            html += NEW_LINE
+        #render all child blocks if available
+        for child_block in self.child_blocks.all().order_by(SORT_LEVEL):
             html += child_block.get_html()
-            
-        html += ('' if self.footer_html is None else self.footer_html) + NEW_LINE
-        if self.tag_type != '-1':
+        #if child blocks were rendered, print a NewLine to have footer rendered in next line
+        if self.child_blocks.count() > 0:
+            html += NEW_LINE    
+        html += '' if self.footer_html is None else (self.footer_html + NEW_LINE)
+        if self.tag_type != _MINUS_1:
             html += post_tag + NEW_LINE
+        
+        html += '<!-- End [{0}] Block -->'.format(self.name) + NEW_LINE
         return html
         
     class Meta:
@@ -238,11 +312,11 @@ class TemplateBlockModel(iot.BaseModel):
 
 class TemplateModel(iot.BaseModel):    
     TEMPLATE_TYPES=(
-            ('-1', 'None'),
-            ('00', 'File'),
-            ('01', 'Runtime'),
+            (_MINUS_1, NONE),
+            (_00, FILE),
+            (_01, RUNTIME),
             )
-    template_type = models.CharField(max_length=2, choices=TEMPLATE_TYPES, default='00')
+    template_type = models.CharField(max_length=2, choices=TEMPLATE_TYPES, default=_00)
     template_path = models.FilePathField(path=os.path.join(BASE_DIR, 'gui/templates'), recursive=True, allow_folders=False, blank=True, null=True)
     meta_tags = models.ManyToManyField(tags.HtmlTagModel, related_name='+', blank=True, default=None, limit_choices_to={'tag_name__contains':'Meta'})
     page_icon_tag = models.ForeignKey(tags.IconTagModel, models.SET_NULL, blank = True, null = True, default=None)
@@ -257,34 +331,34 @@ class TemplateModel(iot.BaseModel):
         return '{0} ({1})'.format(self.name, dict(self.TEMPLATE_TYPES)[self.template_type])
 
     def get_html(self, request, context):
-        if self.template_type == '00':
+        if self.template_type == _00:
             return shortcuts.render(request, self.template_path.lstrip('\\'), context)
         
-        if self.template_type == '01':
+        if self.template_type == _01:
             #start HTML tag
             html = TEMPLATE_NAME.format(self.name)
             html += HTML_START        
             #start Head tag
             html += HEAD_START        
             #Render all meta tags if available        
-            for meta_tag in self.meta_tags.all().order_by('sort_level'):
+            for meta_tag in self.meta_tags.all().order_by(SORT_LEVEL):
                 html += meta_tag.get_html()
                 
             html += NEW_LINE
             
             #Render page-icon-tag
-            html += self.page_icon_tag.get_html('page_icon') + NEW_LINE        
+            html += self.page_icon_tag.get_html(PAGE_ICON) + NEW_LINE        
             #Render title-tag 
             html += self.title_tag.get_html() + NEW_LINE        
             #Render header-script-tags
-            if self.script_tags.filter(position='00').exists():
-                for script_tag in self.script_tags.filter(position='00').order_by('sort_level'):
+            if self.script_tags.filter(position=_00).exists():
+                for script_tag in self.script_tags.filter(position=_00).order_by(SORT_LEVEL):
                     html += script_tag.get_html()
                 
                 html += NEW_LINE
             
             #Render header-style-tags        
-            for style_tag in self.style_tags.all().order_by('sort_level'):
+            for style_tag in self.style_tags.all().order_by(SORT_LEVEL):
                 html += style_tag.get_html()
             
             html += NEW_LINE
@@ -294,14 +368,14 @@ class TemplateModel(iot.BaseModel):
             #Body start tag
             html += BODY_START + NEW_LINE        
             #Render all blocks        
-            for block_tag in self.block_tags.all().order_by('sort_level'):
+            for block_tag in self.block_tags.all().order_by(SORT_LEVEL):
                 html += block_tag.get_html()
             
             html += NEW_LINE
 
             #Render body script tags
-            if self.script_tags.filter(position='01').exists():
-                for body_script in self.script_tags.filter(position='01').order_by('sort_level'):
+            if self.script_tags.filter(position=_01).exists():
+                for body_script in self.script_tags.filter(position=_01).order_by(SORT_LEVEL):
                     html += body_script.get_html()
 
                 html += NEW_LINE
@@ -323,11 +397,11 @@ class ControlModel(UIElementModel):
     
 class ControlStyleModel(iot.BaseModel):
     STYLES=(
-            ('-1', 'None'),
-            ('00', 'File'),
-            ('01', 'Inline'),
+            (_MINUS_1, NONE),
+            (_00, FILE),
+            (_01, INLINE),
             )
-    style_type = models.CharField(max_length=2, choices=STYLES, default='01')
+    style_type = models.CharField(max_length=2, choices=STYLES, default=_01)
     style_path = models.FilePathField(path=os.path.join(BASE_DIR, 'staticfiles/css'), recursive=True, allow_folders=False, blank=True, null=True)
     style_applied_on = models.ForeignKey(ControlModel, models.SET_NULL, blank = True, null = True)
     author = models.ForeignKey(User, models.SET_NULL, blank = True, null = True)
